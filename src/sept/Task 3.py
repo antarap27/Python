ca44bc73e4af4718184e2c78dbef639b544a235e
antara patil <antara.patil@spinny.com>
2024-09-03 22:37:47 +0530

    first file

---
+++ src/sept/Task 3.py
@@ -0,0 +1,20 @@
+# Explain the difference between the = operator and the == operator in Python.
+# What does the ** operator do in Python, and how is it used?
+# What does the ^ operator do in Python, and in what context is it commonly used?
+
+# ********************************************************************************
+
+# = is an assignment operator, for ex: here 2 is assigned to num named variable
+num = 2
+print(f"2 is assigned to num variable\n")
+
+# == is a comparision operator, for ex: here comparing two nos num1 and num2
+
+num1 = 1
+num2 = 1
+
+print(f"num1 and num2 are compared")
+if num1 == num2:
+    print(f"both are equal")
+else:
+    print(f"both are not equal")
